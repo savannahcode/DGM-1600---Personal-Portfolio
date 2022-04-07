b3508e4c586884e04cef7ceb78a4f28a0a573914
savsalmon <savannah.b.hilton@gmail.com>
2022-04-07 16:33:30 -0600

    having trouble getting starship list to work

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -5,9 +5,16 @@ const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipViewer')
 
 function populateNav(){
+    starships.forEach((starship) => {
+        const listItem = document.createElement('li')
+        listItem.textContent = starship.name
 
+        anchorWrap.addEventListener('click', () => populateShipView(starship))
+
+        navList.appendChild(listItem)
+    })
 } 
 
 function populateShipView(){
-    
+    console.log('Thanks for clicking on...', shipData.name)
 }
