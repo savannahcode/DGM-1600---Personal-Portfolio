3ff3cb822772ecda98c0e2dbb2561be56f0d6d0d
savsalmon <savannah.b.hilton@gmail.com>
2022-04-07 20:54:08 -0600

    fixed bug! the ships are showing

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,11 +1,14 @@
-import { starships } from "../data/starships";
+import { starships } from "../data/starships.js";
 
+console.log("Is this working??")
 const nav = document.querySelector('.nav')
 const navList = document.querySelector('.navList')
 const shipView = document.querySelector('.shipViewer')
 
 function populateNav(){
     starships.forEach((starship) => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
         const listItem = document.createElement('li')
         listItem.textContent = starship.name
 
@@ -18,3 +21,5 @@ function populateNav(){
 function populateShipView(){
     console.log('Thanks for clicking on...', shipData.name)
 }
+
+populateNav()
