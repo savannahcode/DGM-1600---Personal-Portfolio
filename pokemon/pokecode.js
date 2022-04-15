ba5a87db0ea3e950cdf8a0bd8b4b462eabb04987
savsalmon <savannah.b.hilton@gmail.com>
2022-04-15 09:11:38 -0600

    continued to work on loadPokemon function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -9,21 +9,51 @@ const getAPIData = async (url) => {
     }
 }
 
-async function loadPokemon(){
-    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`)
-    populatepokeGrid(pokeData)
+async function loadPokemon(offset = 0, limit = 25){
+    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    const pokeResults = pokeData.results // this should be an array of name and url key/value pairs
+    for ( const nameAndURL of pokeResults){
+        const pokemon = await getAPIData(nameAndURL.url)
+        populatePokeCard(pokemon)
+    }
 }
 
-loadPokemon();
+const pokeGrid = document.querySelector('.pokeGrid')
 
-function populatepokeGrid(pokemonArray) {
+/* function populatepokeGrid(pokemonArray) {
     //loop through all of the pokemon and create induvidual pokeCards
-    populatePokeCard(pokemon)
-}
+    //populatePokeCard(pokemonArray[0])
+    console.log(pokemonArray)
+} */
 
 function populatePokeCard(pokemon) {
     const pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     const pokeCard = document.createElement('div')
     pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+    
+
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
 }
+
+function populateCardFront(pokemon){
+    const pokeFront = document.createElement('figure')
+    pokeFront.className = 'cardFace'
+    const pokeImg = document.createElement('img')
+    pokeImg.src = 'https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/official-artwork/143.png'
+    const pokeCaption = document.createElement('figCaption')
+    pokeCaption.textContent = 'snorlax'
+
+    pokeFront.appendChild(pokeImg)
+    pokeFront.appendChild(pokeCaption)
+    return pokeFront
+}
+
+function populateCardBack(pokemon){
+
+}
+
+loadPokemon()
