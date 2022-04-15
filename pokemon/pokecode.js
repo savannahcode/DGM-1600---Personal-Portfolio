eb27a0eeb42b4b031767214fe0e026f35c9bcb91
savsalmon <savannah.b.hilton@gmail.com>
2022-04-14 21:52:11 -0600

    refactored pokecode.js & started other function

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,17 +1,29 @@
 
 
-function getAPIData(url) {
+const getAPIData = async (url) => {
     try{
-        return fetch(url).then((data) => data.json())
+        const result = await fetch(url)
+        return await result.json()
     } catch(error) {
     console.error(error);
     }
 }
 
-function loadPokemon(){
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`).then(
-        (data => console.log(data))
-    )
+async function loadPokemon(){
+    const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon/snorlax`)
+    populatepokeGrid(pokeData)
 }
 
 loadPokemon();
+
+function populatepokeGrid(pokemonArray) {
+    //loop through all of the pokemon and create induvidual pokeCards
+    populatePokeCard(pokemon)
+}
+
+function populatePokeCard(pokemon) {
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+}
