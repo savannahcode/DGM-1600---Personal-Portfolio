22f28688316c097a6634bd90f2d2dcdcc4f21027
savsalmon <savannah.b.hilton@gmail.com>
2022-05-01 23:55:07 -0600

    Added Ep num & release date to film figcaption

--- films/starwars-films.js
+++ films/starwars-films.js
@@ -14,17 +14,27 @@ for (let i = 0; i < films.length; i++) {
 let figure = document.createElement('figure')
 let figImg = document.createElement('img')
 let figCaption = document.createElement('figcaption')
+let movieName = document.createElement('figcaption')
+let releaseDate = document.createElement('p')
+let epidsodeID = document.createElement('p')
+
 
 // set img's source property to valid url
 let filmNum = getLastNumber(films[i].url)
 figImg.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
 
-figCaption.textContent = films[i].title
+movieName.textContent = films[i].title
+releaseDate.textContent = films[i].release_date
+epidsodeID.textContent = " Episode " + films[i].episode_id
 
 
 // append newly created img element as a child of the main element to make it appear in the DOM
 figure.appendChild(figImg)
 figure.appendChild(figCaption)
+figCaption.appendChild(movieName)
+figCaption.appendChild(releaseDate)
+figCaption.appendChild(epidsodeID)
+
 
 filmList.appendChild(figure)
 }
